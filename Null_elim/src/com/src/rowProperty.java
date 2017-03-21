35cc6b4666dd6566a2d8ef23ad563246bcd0019f
Bhavana Shah <Bhavana Shah>
2017-03-21 18:44:01 -0500

    first commit

---
+++ Null_elim/src/com/src/rowProperty.java
@@ -0,0 +1,28 @@
+package com.src;
+
+import java.util.ArrayList;
+
+public class rowProperty {
+
+ ArrayList<tableValues> rowcol;
+ 
+ ArrayList<ConfidenceList> confList;
+ 
+public ArrayList<tableValues> getRowcol() {
+	return rowcol;
+}
+public void setRowcol(ArrayList<tableValues> rowcol) {
+	this.rowcol = rowcol;
+}
+public ArrayList<ConfidenceList> getConfList() {
+	return confList;
+}
+public void setConfList(ArrayList<ConfidenceList> confList) {
+	this.confList = confList;
+}
+
+ 
+
+ 
+
+}
---
+++ Null_elim/src/com/src/tableValues.java
@@ -0,0 +1,28 @@
+package com.src;
+
+import java.util.ArrayList;
+
+public class tableValues {
+
+private String columnName;
+private String columnValue;
+
+
+
+public String getColumnName() {
+	return columnName;
+}
+public void setColumnName(String columnName) {
+	this.columnName = columnName;
+}
+public String getColumnValue() {
+	return columnValue;
+}
+public void setColumnValue(String columnValue) {
+	this.columnValue = columnValue;
+}
+
+
+
+
+}
